edd64fc7dad4d958bcdb86970e5ef5ebdd451b55
Tamara Erby <tlynnerby@yahoo.com>
2021-08-11 20:38:45 -0400

    initial commit

--- SocialMedia.Data/Reply.cs
+++ SocialMedia.Data/Reply.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Data
 {
-    class Reply
+    public class Reply
     {
+        [Key]
+        public int ReplyID { get; set; }
+
+        [Required]
+        public Guid AuthorID { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
     }
 }
