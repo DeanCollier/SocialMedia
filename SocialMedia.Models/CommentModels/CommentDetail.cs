6c226561f74ee4d8817a372d529d0e8e4967b168
Dean Collier <daccollier@gmail.com>
2021-08-11 20:16:19 -0400

    small changes

--- SocialMedia.Models/CommentModels/CommentDetail.cs
+++ SocialMedia.Models/CommentModels/CommentDetail.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,12 @@ using System.Threading.Tasks;
 namespace SocialMedia.Models.CommentModels
 {
     public class CommentDetail
-    {
+    {   
+        [Required]
+        public int CommentId { get; set; }
+
+        public string CommentText { get; set; }
+
+
     }
 }
