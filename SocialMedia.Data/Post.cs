d4d22f4cf7f77bab2535445ba74b56ab8aeac18d
Mariah Page <mariahlynnpage@gmail.com>
2021-08-11 20:11:02 -0400

    new commit

--- SocialMedia.Data/Post.cs
+++ SocialMedia.Data/Post.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SocialMedia.Data
 {
-    class Post
+    public class Post
     {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        public Guid AuthorId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+        [Required]
+        public virtual List<string> Comments { get; set; }
+        public virtual List<string> Likes { get; set; }
+
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }
--- SocialMedia.WebAPI/Controllers/PostController.cs
+++ SocialMedia.WebAPI/Controllers/PostController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models.PostModels;
+using SocialMedia.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,7 +10,62 @@ using System.Web.Http;
 
 namespace SocialMedia.WebAPI.Controllers
 {
+    [Authorize]
     public class PostController : ApiController
     {
+        public IHttpActionResult Get()
+        {
+            PostService postService = CreatePostService();
+            var posts = postService.GetPosts();
+            return Ok(posts);
+        }
+        public IHttpActionResult Post(PostCreate post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.CreateNote(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private PostService CreatePostService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var postService = new PostService(userId);
+            return postService;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            PostService postService = CreatePostService();
+            var post = postService.GetPostById(id);
+            return Ok(post);
+        }
+
+        public IHttpActionResult Put(PostEdit post)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePostService();
+
+            if (!service.UpdatePost(post))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreatePostService();
+
+            if (!service.DeletePost(id))
+                return InternalServerError();
+
+            return Ok();
+        }
     }
 }
