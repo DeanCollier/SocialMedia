f7912b13f1c902ec7c72f0cd6f4b35551487e87a
Dean Collier <daccollier@gmail.com>
2021-08-12 21:09:50 -0400

    wowzers

--- SocialMedia.Models/PostModels/PostDetail.cs
+++ SocialMedia.Models/PostModels/PostDetail.cs
@@ -1,4 +1,5 @@
 ﻿using SocialMedia.Data;
+using SocialMedia.Models.CommentModels;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -15,7 +16,7 @@ namespace SocialMedia.Models.PostModels
         public Guid AuthorId { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public virtual List<Comment> Comments { get; set; }
+        public virtual IEnumerable<CommentNoPost> Comments { get; set; }
         public virtual List<Like> Likes { get; set; }
 
         public DateTimeOffset CreatedUtc { get; set; }
